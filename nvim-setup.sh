b8ec93950dff7f519beb85cd8af39f26194981ed
Ilya Naydanov <turbcool@gmail.com>
2022-12-30 02:12:04 +0500

    Create nvim-setup.sh

---
+++ nvim-setup.sh
@@ -0,0 +1,7 @@
+mkdir -p ~/.config/nvim
+curl https://raw.githubusercontent.com/nvim-lua/kickstart.nvim/master/init.lua -o ~/.config/nvim/init.lua
+
+# nvim [Enter]
+# nvim
+
+# :Mason; ember-language-server -> i
