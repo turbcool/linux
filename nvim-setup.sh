fbf7826ecac7815ce6c9aad2603171e0d210d25f
Ilya Naydanov <turbcool@gmail.com>
2023-01-09 20:38:38 +0500

    Update nvim-setup.sh

--- nvim-setup.sh
+++ nvim-setup.sh
@@ -8,7 +8,12 @@ curl https://raw.githubusercontent.com/nvim-lua/kickstart.nvim/master/init.lua -
 # :Mason; ember-language-server -> i
 
 # AstroVim:
-#sudo zypper install ripgrep lazygit
+sudo zypper install ripgrep
+mv ~/.config/nvim ~/.config/nvim.bak
+git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+
+# Install Nerd Fonts:
+# https://www.nerdfonts.com/font-downloads
 
 # Go + lazygit:
 sudo zypper install go
