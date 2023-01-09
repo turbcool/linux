03c0858164578fc116c41cbb4c8ca6d11e0f8ec7
Ilya Naydanov <turbcool@gmail.com>
2023-01-10 04:33:36 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,15 @@
 git config --global push.default current
 
 
-# Yandex Disk
+# Yandex Disk (source: https://nkonev.name/post/90)
 sudo zypper install davfs2
 sudo usermod -aG davfs2 turb
 sudo mkdir -p /run/media/turb/yandex.ivnaidanov
 sudo chown -R turb:turb /run/media/turb/yandex.ivnaidanov
 chmod 777 /run/media/turb/yandex.ivnaidanov
-echo "/run/media/turb/yandex.ivnaidanov ivnaidanov@hse.ru egqopzpuhkdieybq" | sudo tee -a /etc/davfs2/secrets
 echo "https://webdav.yandex.ru /run/media/turb/yandex.ivnaidanov davfs noauto,user 0 0" | sudo tee -a /etc/fstab
+
+# Davfs2 secrets:
+mkdir ~/.davfs2
+echo "/run/media/turb/yandex.ivnaidanov ivnaidanov@hse.ru zwmfoqaiyyzhufeg" | sudo tee -a ~/.davfs2/secrets
 chmod 600 ~/.davfs2/secrets
