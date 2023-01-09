857a4bd59a536bd209dd9d72118dc0b5f6cb7edc
Ilya Naydanov <turbcool@gmail.com>
2023-01-09 20:37:09 +0500

    Update nvim-setup.sh

--- nvim-setup.sh
+++ nvim-setup.sh
@@ -1,3 +1,4 @@
+# Kickstart:
 mkdir -p ~/.config/nvim
 curl https://raw.githubusercontent.com/nvim-lua/kickstart.nvim/master/init.lua -o ~/.config/nvim/init.lua
 
@@ -5,3 +6,10 @@ curl https://raw.githubusercontent.com/nvim-lua/kickstart.nvim/master/init.lua -
 # nvim
 
 # :Mason; ember-language-server -> i
+
+# AstroVim:
+#sudo zypper install ripgrep lazygit
+
+# Go + lazygit:
+sudo zypper install go
+go install github.com/jesseduffield/lazygit@latest
